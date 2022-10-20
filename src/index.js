2cc867a844c26354e6a60d81a1846a564d89f7c5
Vanecmorales <vanessagm.91@gmail.com>
2022-10-20 13:58:04 -0300

    filtrar links

--- src/index.js
+++ src/index.js
@@ -3,30 +3,26 @@
 const fs = require("fs"); //file system
 const path = require("path");
 const colors = require("colors");
+const markdownLinkExtractor = require("markdown-link-extractor");
+const linkCheck = require("link-check");
 
 // Estos son los parametros de la función mdLinks
 const userRoute = process.argv[2]; // guarda la ruta ingresada por el usuario
 const option = process.argv[3]; // guarda la opción ingresada por el usuario
 
 // 1- Verificar si la ruta es absoluta, si no lo es cambiar a absoluta
-const absoluteRoute = 
+const absoluteRoute =
   path.isAbsolute(userRoute) === true ? userRoute : path.resolve(userRoute);
-console.log("1-La ruta aboluta es: ".green, absoluteRoute.yellow);
+console.log("1-La ruta aboluta es: ".cyan, absoluteRoute.yellow);
 
 // 2- Comprobar si la ruta ingresada existe
 const routeExist = () => fs.existsSync(absoluteRoute);
-console.log("2-¿Es una ruta real?: ".green, routeExist());
+console.log("2-¿Es una ruta real?: ".cyan, routeExist());
 
 // 3- Verificar si es un archivo o directorio
 const fileOrDirectory = () => {
-  const file = fs.statSync(absoluteRoute).isFile();  
-  file === true ? console.log("3-Sí, esto es un archivo".rainbow) : console.log("3-Esto es un directorio".red)
-  // const stats = fs.statSync(absoluteRoute);
-  // const file = stats.isFile();
-    // if (file === true){
-  //   return console.log("3-Sí es un archivo".rainbow)
-  // }
-  // return console.log("3-Lo siento, esto es un directorio".red)
+  const file = fs.statSync(absoluteRoute).isFile();
+  file === true ? console.log("3-Sí, esto es un archivo".rainbow) : console.log("3-Esto es un directorio".red);
 };
 fileOrDirectory(absoluteRoute);
 
@@ -34,7 +30,7 @@ fileOrDirectory(absoluteRoute);
 const fileExtension = () => {
   return path.extname(absoluteRoute) === ".md";
 };
-console.log("4-¿La extensión del archivo es md?".green, fileExtension());
+console.log("4-¿La extensión del archivo es md?".cyan, fileExtension());
 
 // 5- Buscar archivos .md en un directorio
 const readDirectory = (absoluteRoute) => {
@@ -54,17 +50,42 @@ const readDirectory = (absoluteRoute) => {
     });
   });
 };
-console.log("5-Archivos encontrados".cyan, readDirectory(absoluteRoute));
 
-// 0- Leer el archivo
-// const readContent = fs.readFile(absoluteRoute, "utf-8", (error, data) => {
-//     if (error) {
-//       console.log("0-Ha ocurrido un error: ".red, error);
+// 6- Extraer links de archivos .md (con la librería)
+
+const extractLinks = (absoluteRoute) => {
+  const markdownFile = fs.readFileSync(absoluteRoute, { encoding: "utf-8" });
+  const { links } = markdownLinkExtractor(markdownFile);
+  console.log("Estos son los links: ", links);
+  return links;
+};
+
+// 6.1- Muestras todos los links en un solo arreglo
+let urlResult = [];
+readDirectory(absoluteRoute)
+  .then((files) => {
+    files.forEach((file) => {
+      const rutaAbsolutaArchivo = path.resolve(__dirname, "test-files", file);
+      console.log({ rutaAbsolutaArchivo });
+      urlResult = [...urlResult, ...extractLinks(rutaAbsolutaArchivo)];
+      //spread operator: permite que los elementos de uno o más arreglos se unan en uno nuevo
+    });
+  })
+  .finally(() => {
+    console.log({ urlResult });
+  });
+
+
+// // 7- funcion que checkea el estado de los links
+// const checkLinks = () => {
+//   linkCheck('https://github.com/Vanecmorales/SCL021-cipher', function (err, result) {
+//     if (err) {
+//         console.error(err);
+//         return;
 //     }
-//     console.log("0-El contenido del archivo es: ".green, data.red);
-//   }
-// );
-// console.log(readContent);
+//     console.log(`${result.link} is ${result.status}`);
+// });
+// }
 
 //index.js: Desde este archivo debes exportar una función (mdLinks).
 // const mdLinks = (absoluteRoute, option) => {
---
+++ src/test-files/repositorios.md
@@ -0,0 +1,6 @@
+## 1. Repositorios de Laboratoria
+
+Cifrado cesar: https://github.com/Vanecmorales/SCL021-cipher
+Data Lovers: https://github.com/Vanecmorales/SCL021-data-lovers
+Social network: https://github.com/Vanecmorales/SCL021-social-network
+MDLinks: https://github.com/Vanecmorales/SCL021-md-links
